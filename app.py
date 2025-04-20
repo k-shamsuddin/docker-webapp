32b3ac5cd45d1baa2ccac7829786ed1830ad2e03
Khaja Shamsuddin Ahmed <k.shamsuddin.a@gmail.com>
2025-04-20 12:09:20 +0000

    app.py updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def home():
-    return "Hello, im khaja shamsuddin!. This is a Dockerized Flask App!"
+    return "Hello, this is a Dockerized Flask App! by Shams."
 
 if __name__ == "__main__":
 
